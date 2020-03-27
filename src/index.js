14024464c041d9e8aafa2ed2144f02cf192e9c86
Egor Malyutin <egorcod.tk@gmail.com>
2020-03-27 21:59:48 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -6,52 +6,107 @@ const { R_OK } = require("fs").constants
 
 const webpack = require("webpack")
 
-const generateConfig = require("./generate-config")
-
-// TODO: entry as args, help, examples
-// TODO: multiple dist
+// TODO: help, examples
+// TODO: detect index.ts or index.html or index.js
+// TODO: gcc, custom min plugin
+// TODO: public path, externals, externals regexp
+// TODO: env
+// TODO: default tsconfig
+// TODO: append, prepend
+// TODO: disable/only assets
+// TODO: main.js, glob
 
 const parser = require("yargs")
     .detectLocale(false)
 
     .usage("Usage: $0 [options]")
 
+    .group(
+        [
+            "entry",
+            "entry-output",
+            "dist",
+            "production",
+            "tsconfig",
+            "inspect",
+            "single-js",
+            "library-target",
+            "devtool"
+        ],
+        "Webpack options:"
+    )
+
     .option("entry", {
         alias: "e",
         type: "string",
-        description: "Entry file (can be specified multiple times)"
+        description:
+            "Entry file (can be specified multiple times, " +
+            "default is src/index.ts or src/index.js)",
+        default: []
     })
 
     .array("entry")
 
+    .option("entry-output", {
+        alias: "eo",
+        type: "string",
+        description:
+            "Entry file with output bundle name " +
+            "(accepts 2 arguments, can be specified multiple times)",
+        default: []
+    })
+
+    .array("entry-output")
+    .nargs("entry-output", 2)
+
     .option("dist", {
         alias: "d",
         type: "string",
-        description: "Dist dir"
+        description: "Dist dir",
+        default: "dist"
     })
 
-    .option("development", {
-        alias: "dev",
+    .option("production", {
+        alias: "prod",
         type: "boolean",
-        description: "Development mode (production mode is default)"
+        description: "Production mode (development mode is default)",
+        default: false
     })
 
     .option("tsconfig", {
         alias: "ts",
         type: "string",
-        description: "Path to tsconfig.json"
+        description: "Path to tsconfig.json",
+        default: "tsconfig.json"
     })
 
     .option("inspect", {
         alias: "in",
         type: "boolean",
-        description: "Inspect Webpack config"
+        description: "Inspect Webpack config",
+        default: false
     })
 
     .option("single-js", {
-        alias: "sj",
+        alias: "sjs",
         type: "boolean",
-        description: "Generate single minified .js in production mode"
+        description: "Generate single minified .js in production mode",
+        default: false
+    })
+
+    .option("library-target", {
+        alias: "lt",
+        type: "string",
+        description: "libraryTarget Webpack option",
+        default: "var"
+    })
+
+    .option("devtool", {
+        alias: "dt",
+        type: "string",
+        description:
+            "devtool Webpack option (by default in production is " +
+            "none, in development is source-map)"
     })
 
     .alias("h", "help")
@@ -89,29 +144,78 @@ const exists = async pth => {
     }
 }
 
-const generateSettings = async opts => {
-    settings = {
-        production: !opts.development,
-        entries: opts.entry,
-        dist: opts.dist || "dist",
-        tsconfig: opts.tsconfig,
-        singleJS: opts["single-js"]
+;(async () => {
+    // ENTRIES
+    const entries = {}
+
+    // Add entries from --entry
+    for (const entry of opts.entry) {
+        entries[path.basename(entry, path.extname(entry))] = entry
     }
 
-    if (!settings.entries || settings.entries.length == 0) {
+    // Add entries from --entry-output
+    for (let i = 0; i < opts.entryOutput.length; i += 2) {
+        entries[opts.entryOutput[i + 1]] = opts.entryOutput[i]
+    }
+
+    // Add default entries
+    if (Object.keys(entries).length == 0) {
         if (await exists(path.resolve("src/index.ts"))) {
-            settings.entries = [path.resolve("src/index.ts")]
+            entries["index"] = path.resolve("src/index.ts")
         } else {
-            settings.entries = [path.resolve("src/index.js")]
+            entries["index"] = path.resolve("src/index.js")
         }
     }
 
-    return settings
-}
+    // Add .min outputs
+    if (opts.production && !opts.singleJs) {
+        for (const name in entries) {
+            entries[name + ".min"] = entries[name]
+        }
+    }
 
-;(async () => {
-    const settings = await generateSettings(opts)
-    const config = generateConfig(settings)
+    // GENERATE CONFIG
+    const config = {
+        mode: opts.production ? "production" : "development",
+
+        entry: entries,
+
+        output: {
+            path: path.resolve(opts.dist),
+            filename: "[name].js",
+            libraryTarget: opts.libraryTarget
+        },
+
+        resolve: {
+            modules: [".", "node_modules"],
+            extensions: [".ts", ".tsx", ".js", ".jsx"]
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    options: {
+                        configFile: opts.tsconfig
+                    }
+                }
+            ]
+        },
+
+        devtool: opts.devtool || opts.production ? "none" : "source-map",
+
+        optimization: {
+            minimize: opts.production,
+            minimizer: opts.production
+                ? [
+                      new (require("terser-webpack-plugin"))({
+                          include: opts.singleJs ? /\.js$/ : /\.min\.js$/
+                      })
+                  ]
+                : []
+        }
+    }
 
     if (opts.inspect) {
         console.log(inspect(config, { depth: 999999, colors: true }))
